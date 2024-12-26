2b122735ea8477d315792a91801b44ce729bb892
Tanzeebul Tamim <tamim200091@gmail.com>
2024-12-27 01:29:10 +0600

    Type writer effect added in intro title

--- src/utilities/ConsoleUtils.java
+++ src/utilities/ConsoleUtils.java
@@ -1,11 +1,24 @@
 package utilities;
 
+import java.util.Random;
+
 public final class ConsoleUtils extends BaseUtils {
     // Method to print title
-    public static void printTitle() {
+    public static void printTitle() throws InterruptedException {
+        String title1 = "Welcome to the '";
+        String title2 = "Assignment File Generator";
+        String title3 = "' Automate, Organize, & Submit with Ease!";
+
         clearConsole();
-        System.out.println("Welcome to the Assignment File Generator Tool!");
-        System.out.println("You can press Ctrl + C anytime to terminate the program.\n");
+        typewriter(title1, title2, title3, 6);
+
+        Thread.sleep(1000);
+        System.out.println("\n");
+
+        System.out.println("YYou can press Ctrl + C anytime to terminate the program.\n");
+        Thread.sleep(3000);
+        clearConsole();
+
         System.out.println(
                 "The output file will be saved using the following naming format: `Assignment XX_Your-ID_Your-Name`");
         System.out.println("(e.g., Assignment 03_24100000_Joe Brooks.txt)\n");
@@ -37,6 +50,87 @@ public final class ConsoleUtils extends BaseUtils {
         System.out.println(message);
         Thread.sleep(waitTime); // Wait for 'waitTime' seconds
         ConsoleUtils.clearConsole();
+    }
+
+    // Simulates Typewriter effect
+    private static void typewriter(
+            String introText,
+            String mainContent,
+            String outroText,
+            int milliseconds)
+            throws InterruptedException {
+
+        String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        // Prepare the full title (intro + main + outro)
+        String fullTitle = introText + " " + mainContent + " " + outroText;
+
+        // Build the underline according to the string length
+        String underline1 = "_".repeat(fullTitle.length());
+        String underline2 = "-".repeat(fullTitle.length());
+
+        Thread.sleep(550);
+
+        // Print First underline
+        System.out.println(underline1);
+        System.out.println(underline2);
+
+        // Print Empty line for spacing
+        System.out.println("");
+
+        // Print Second underline
+        System.out.println(underline1);
+        System.out.println(underline2);
+
+        // Move cursor up
+        System.out.print("\033[A");
+        System.out.print("\033[A");
+        System.out.print("\033[A");
+
+        Thread.sleep(550);
+
+        // Typing simulation for the title (intro + main + outro)
+        StringBuilder titleBuilder = new StringBuilder();
+        // Type intro text
+        if (!introText.isEmpty()) {
+            for (String word : introText.split(" ")) {
+                System.out.print(word + " ");
+                Thread.sleep(500); // 1-second interval between words
+            }
+        }
+
+        // Type main content with random characters (typewriter effect)
+        for (char letter : mainContent.toCharArray()) {
+            while (true) {
+                char randomChar = letters.charAt(new Random().nextInt(letters.length()));
+                System.out.print("\r" + introText + titleBuilder + randomChar);
+
+                if (letter == randomChar) {
+                    // Appending the letters
+                    titleBuilder.append(letter);
+                    break;
+                } else if (letter == ' ' || letter == '-' || letter == ',' || letter == '\'' || letter == '!') {
+                    // Appending other necessary characters
+                    titleBuilder.append(letter);
+                    break;
+                }
+
+                Thread.sleep(milliseconds); // Adjust typing speed
+            }
+        }
+
+        // Type outro text
+        if (!outroText.isEmpty()) {
+            for (String word : outroText.split(" ")) {
+                System.out.print(word + " ");
+                Thread.sleep(500); // 1-second interval between words
+            }
+        }
+
+        // Move cursor down
+        System.out.print("\033");
+        System.out.print("\033");
+        System.out.print("\033 ");
     }
 
     // Method to halt the program until user enters something
